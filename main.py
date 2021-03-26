0b7cdd9b2499207646f58396b0ab0900ba3c3c44
Tobias Powelske <FirimaElda@gmail.com>
2021-03-26 23:02:49 +0100

    Scraping for car prices over result pages implemented. Functionality for scraping result pages implemented.

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 import argparse
 import mobile.singleresult as sr
 import mobile.singleresult_selenium as srs
+import re
+
+
+def get_next_result_page_url(resulturl):
+    nextpageattr = re.search(r'pageNumber=(\d*)', resulturl)
+    print(nextpageattr)
+    if nextpageattr is None:
+        nextpageattr = 1
+        resulturl += '&pageNumber=1'
+    else:
+        nextpageattr = nextpageattr.group(1)
+    nextpagenr = int(nextpageattr) + 1
+    print(nextpagenr)
+    nextpageurl = re.sub(r'pageNumber=(\d*)', 'pageNumber=' + str(nextpagenr), resulturl)
+    print(nextpageurl)
 
 
 if __name__ == '__main__':
     # setup the arguments...
     parser = argparse.ArgumentParser()
     parser.add_argument('result_URL', help='The URL of the results of the cars to be scraped.')
-    #parser.add_argument('--alpha', action='store_true')
-    #parser.add_argument('--plot', action='store_true')
-    #parser.add_argument('--stock', help='The symbol of the stock to get. Only necessary if alpha-switch is on.')
     args = parser.parse_args()
     carres = sr.extract_results(args.result_URL)
     print('-----------------------------------------------------')
-    for car in carres:
-        print(car.prettify())
-        print('-----------------------------------------------------')
-
-    print(len(carres))
-    srs.extract_results(args.result_URL)
+    print('Cars on first page: ' + str(len(carres)))
+    print('-----------------------------------------------------')
+    print('Getting prices of cars...')
+    srs.get_prices_from_results_url(args.result_URL)
+    if False:
+        print('Building list of cars...')
+        currurl = args.result_URL
+        iternr = 0
+        carurllist = []
+        while currurl is not None:
+            iternr += 1
+            print('Getting cars of page ' + str(iternr) + '...')
+            currcars = srs.extract_results(currurl)
+            print('Number of cars on page: ' + str(len(currcars)))
+            carurllist.extend(currcars)
+            currurl = srs.get_next_page_url(currurl)
+        print('Done building list. Got ' + str(len(carurllist)) + ' car URLs.')
+        print('Working through scraped cars...')
+        pricelist = [srs.get_price_from_offer_url(carurl) for carurl in carurllist]
+        print(pricelist)
--- mobile/singleresult_selenium.py
+++ mobile/singleresult_selenium.py
@@ -1,11 +1,70 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.firefox.options import Options
+import re
 
 
-def extract_results(url):
+def get_driver():
     options = Options()
     options.headless = True
     driver = webdriver.Firefox(options=options)
+    return driver
+
+
+# Returns a list of links from the result page.
+def extract_results(url):
+    driver = get_driver()
     driver.get(url)
-    a = driver.find_element_by_xpath("//*[contains(@href, 'fahrzeuge/details')]")
-    print(a.get_attribute('href'))
+    try:
+        a = driver.find_elements_by_xpath("//*[contains(@href, 'fahrzeuge/details')]")
+        linklist = [links.get_attribute('href') for links in a]
+        return linklist
+    finally:
+        driver.quit()
+
+
+def get_next_page_url(url):
+    driver = get_driver()
+    driver.get(url)
+    try:
+        nexturlelement = driver.find_element_by_xpath("//*[contains(text(), 'Weitere Angebote')]")
+        return nexturlelement.get_attribute('data-href')
+    except NoSuchElementException:
+        print('No more cars in this search!')
+        return None
+    finally:
+        driver.quit()
+
+
+# Takes the URL of a car search and returns a list of prices for the offers on that search page.
+def get_prices_from_results_url(url):
+    driver = get_driver()
+    driver.get(url)
+    try:
+        priceelements = driver.find_elements_by_xpath("//div[contains(@class, 'price-block')]")
+        pricelist = [re.search(r'(\d*\.\d*) €', price.text) for price in priceelements]
+        pricelistint = [int(re.sub(r'\.', '', pricere.group(1))) for pricere in pricelist if pricere is not None]
+        print(len(pricelist))
+        print(pricelist)
+        print(pricelistint)
+        return priceelements
+    except NoSuchElementException:
+        print('No price elements found!')
+        return None
+    finally:
+        driver.quit()
+
+
+# Gets a URL to a car page and returns the price of it.
+def get_price_from_offer_url(url):
+    driver = get_driver()
+    driver.get(url)
+    try:
+        priceelement = driver.find_element_by_xpath("//span[@class = 'h3 rbt-prime-price']")
+        print(priceelement.text)
+        return priceelement.text
+    except NoSuchElementException:
+        print('No price element found!')
+        return None
+    finally:
+        driver.quit()
